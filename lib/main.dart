c6b62d9507255ce1237ba92b9221f949f5bf88de
saragalalmoawad <sg1323@fayoum.edu.eg>
2024-05-11 17:52:06 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:mobilecomputing/view/Splash.dart';
+
+void main() {
+ runApp(MyApp());
+ /* runApp(
+    DevicePreview(
+     enabled: !kReleaseMode
+    ,builder: ((context)=>const MyApp()),));*/
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AnimatedSplash(),
+    );
+  }
+}
---
+++ lib/view/Splash.dart
@@ -0,0 +1,25 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:mobilecomputing/view/Splash2.dart';
+
+class AnimatedSplash extends StatelessWidget {
+  const AnimatedSplash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(splash: 
+    Column(
+      children: [
+        Center(
+          child: LottieBuilder.asset("assets/Animation - 1714077249514.json"),
+          
+        )
+      ],
+    )
+     ,nextScreen: Splash2()
+     ,splashIconSize: 400,
+     backgroundColor: Colors.white,
+     );
+  }
+}
